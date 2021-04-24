be4b49e181b600b3f8beef3dc1c4e136aaf993cf
Sara <sara.m.adamski@gmail.com>
2021-04-24 12:51:36 -0500

    save items to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,82 +1,85 @@
+// display date and time in jumbotron
 var displayDate = function() {
-    var currentDay = moment().format('MMMM Do YYYY, h:mm:ss a');
+    var currentDay = moment().format('MMMM Do YYYY, h:mm a');
     var displayDate = document.querySelector("#currentDay");
     displayDate.textContent = currentDay;
 }
 
+// display date on page load
+displayDate();
+
+// update time and color tense every 1 minute
 setInterval(function () {
     displayDate();
-}, 1000);
+    colorCode();
+}, 60000);
 
+// moment.js format to hour, minute, and AM or PM
 var format = 'h:mm A'
 var currentTime = moment();
 
 var timeArray = [
     {time: moment('08:00', format),
-    id: $("#eightAm")
+    id: $("#eightDiv")
     },
     {time: moment('09:00', format),
-    id: $("#nineAm")
+    id: $("#nineDiv")
     },
     {time: moment('10:00', format),
-    id: $("#tenAm")
+    id: $("#tenDiv")
     },
     {time: moment('11:00', format),
-    id: $("#elevenAm")
+    id: $("#elevenDiv")
     },
     {time: moment('12:00', format),
-    id: $("#twelvePm")
+    id: $("#twelveDiv")
     },
     {time: moment('13:00', format),
-    id: $("#onePm")
+    id: $("#oneDiv")
     },
     {time: moment('14:00', format),
-    id: $("#twoPm")
+    id: $("#twoDiv")
     },
     {time: moment('15:00', format),
-    id: $("#threePm")
+    id: $("#threeDiv")
     },
     {time: moment('16:00', format),
-    id: $("#fourPm")
+    id: $("#fourDiv")
     },
     {time: moment('17:00', format),
-    id: $("#fivePm")
+    id: $("#fiveDiv")
     },
 ];
 
-for (var i = 0; i < timeArray.length; i++) {
-    if (currentTime.isAfter(timeArray[i].time)) {
-        $(timeArray[i].id).addClass("past");
-    }
-    if (currentTime.isSame(timeArray[i].time, 'hour')) {
-        $(timeArray[i].id).addClass("present");
-    }
-    if (currentTime.isBefore(timeArray[i].time)) {
-        $(timeArray[i].id).addClass("future");
+// change color of time slot to represent past/present/future
+var colorCode = function() {
+    for (var i = 0; i < timeArray.length; i++) {
+        if (currentTime.isAfter(timeArray[i].time)) {
+            $(timeArray[i].id).addClass("past");
+        }
+        if (currentTime.isSame(timeArray[i].time, 'hour')) {
+            $(timeArray[i].id).addClass("present");
+        }
+        if (currentTime.isBefore(timeArray[i].time)) {
+            $(timeArray[i].id).addClass("future");
+        }
     }
 }
 
-$(".task-edit").on("click", function() {
-    // get text content
-    var text = $(this)
-    .text()
-    .trim();
-    // create textbox
-    var userInput = $("<textarea>")
-    .val(text);
-    // replace task-edit div with newly created textbox
-    $(this).replaceWith(userInput);
+// display task color on page load
+colorCode();
 
-    // if ($(this).text === $(userInput).text) {
-    //     console.log("Yay");
-    // }
-});
+// save items to local storage
+$(".saveBtn").click(function() {
+    var text = $(this).siblings(".task-edit").val();
+    var hour = $(this).siblings(".hour").text();
+    localStorage.setItem(hour, text);
+})
 
-var text = $("#eight").text();
-
-console.log(text);
-
-
-$("#save-8am").click(function() {
-    localStorage.setItem("8 AMtask", JSON.stringify(text));
+// access items from local storage and display on page
+$(".time-block").each(function() {
+    var task = document.querySelector(".task-edit");
+    var hour = document.querySelector(".hour").innerHTML;
+    var displayTask = localStorage.getItem(hour);
+    task.innerHTML = displayTask;
 })
